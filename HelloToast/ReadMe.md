dd6cc681553aec11bc03eb2eece02b90ca532f77
Abhi <abhishek7340128579@gmail.com>
2021-04-27 12:45:09 +0530

    HelloToast App

---
+++ HelloToast/ReadMe.md
@@ -0,0 +1,7 @@
+# My Hello Toast App
+
+
+## Previews of the application
+
+<img title="" src="https://github.com/abhishek123-bit/CDN/blob/main/HelloToastApp/01.png?raw=true" alt="" width="231">
+<img title="" src="https://github.com/abhishek123-bit/CDN/blob/main/HelloToastApp/02.png?raw=true" alt="" width="231">
---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.hellotoast.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding b;
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        b=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+
+         eventHandler();
+    }
+
+    private void eventHandler() {
+
+        //To show the toast message
+        b.btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //To Increase the count value
+        b.btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.count.setText(++count+"");
+            }
+        });
+    }
+}
