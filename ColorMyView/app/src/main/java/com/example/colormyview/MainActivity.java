ef7844e0c2704c104ffe047d5a49f87846c2f25a
Abhi <abhishek7340128579@gmail.com>
2021-05-05 12:42:09 +0530

    JavaDocs Added

--- ColorMyView/app/src/main/java/com/example/colormyview/MainActivity.java
+++ ColorMyView/app/src/main/java/com/example/colormyview/MainActivity.java
@@ -10,43 +10,59 @@ import android.view.View;
 import com.example.colormyview.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-   ActivityMainBinding b;
+    ActivityMainBinding b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        b=ActivityMainBinding.inflate(getLayoutInflater());
+        //Initialize binding
+        b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
 
     }
 
 
-    //Change background color
+    /**
+     * Background color change on click event
+     *
+     * @param view To get which view is click
+     */
     @SuppressLint({"ResourceType", "NonConstantResourceId"})
     public void changeBackground(View view) {
-        switch (view.getId()){
+        //check which view is click
+        switch (view.getId()) {
+            //BoxOne is click
             case R.id.box_one_text:
                 view.setBackgroundColor(Color.DKGRAY);
                 break;
+            //BoxTwo is click
             case R.id.box_two_text:
                 view.setBackgroundColor(Color.GRAY);
                 break;
+            //BoxThree is click
             case R.id.box_three_text:
+                //BoxFive is click
             case R.id.box_five_text:
                 view.setBackgroundColor(Color.BLUE);
                 break;
+            //BoxFour is click
             case R.id.box_four_text:
                 view.setBackgroundColor(Color.MAGENTA);
                 break;
+            //Red Button is click
             case R.id.btn_red:
                 b.boxThreeText.setBackgroundColor(getResources().getColor(R.color.red));
                 break;
+            //Yellow Button is click
             case R.id.btn_yellow:
                 b.boxFourText.setBackgroundColor(getResources().getColor(R.color.yellow));
                 break;
+            //Green Button is click
             case R.id.btn_green:
                 b.boxFiveText.setBackgroundColor(getResources().getColor(R.color.green));
                 break;
+            //click on root
             default:
                 view.setBackgroundColor(Color.LTGRAY);
 
